4d26191c85169859a6c65abf6f5b9c107b7c28de
Kimberly Abacco <kabacco@nimbyx.com>
2018-12-08 21:09:42 +0800

    update

--- src/app/upload.factory.js
+++ src/app/upload.factory.js
@@ -8,100 +8,90 @@
 
         var upload = function (config) {
 
-            initializeState(config).then(function (state) {
+            var state = initializeState(config)
 
-                var reader = new FileReader();
-                reader.onloadend = function (evt) {
-                    if (evt.target.readyState === FileReader.DONE && !state.cancelled) { // DONE == 2
-                        var uri = state.fileUrl + '&comp=block&blockid=' + state.blockIds[state.blockIds.length - 1];
-                        var requestData = new Uint8Array(evt.target.result);
+            var reader = new FileReader();
+            reader.onloadend = function (evt) {
+                if (evt.target.readyState === FileReader.DONE && !state.cancelled) { // DONE == 2
+                    var uri = state.fileUrl + '&comp=block&blockid=' + state.blockIds[state.blockIds.length - 1];
+                    var requestData = new Uint8Array(evt.target.result);
 
-                        $http.put(uri, requestData,
-                            {
-                                headers: {
-                                    'x-ms-blob-type': 'BlockBlob',
-                                    'Content-Type': state.file.type,
-                                },
-                                requestComingFromUploaderService: true,
-                                transformRequest: [],
+                    $http.put(uri, requestData,
+                        {
+                            headers: {
+                                'x-ms-blob-type': 'BlockBlob',
+                                'Content-Type': state.file.type,
+                            },
+                            requestComingFromUploaderService: true,
+                            transformRequest: [],
 
-                            }).then(function (response) {
+                        }).then(function (response) {
 
-                                if (!state.cancelled) {
-                                    state.bytesUploaded += requestData.length;
+                            if (!state.cancelled) {
+                                state.bytesUploaded += requestData.length;
 
-                                    var percentComplete = ((parseFloat(state.bytesUploaded) / parseFloat(state.file.size)) * 100).toFixed(2);
-                                    if (state.progress) state.progress(percentComplete, response.data, response.status, response.headers, response.config);
+                                var percentComplete = ((parseFloat(state.bytesUploaded) / parseFloat(state.file.size)) * 100).toFixed(2);
+                                if (state.progress) state.progress(percentComplete, response.data, response.status, response.headers, response.config);
 
-                                    uploadFileInBlocks(reader, state);
-                                }
+                                uploadFileInBlocks(reader, state);
+                            }
 
-                            }, function (response) {
+                        }, function (response) {
 
-                                if (state.error) state.error(response.data, response.status, response.headers, response.config);
-                            });
-                    }
-                };
+                            if (state.error) state.error(response.data, response.status, response.headers, response.config);
+                        });
+                }
+            };
 
-                uploadFileInBlocks(reader, state);
+            uploadFileInBlocks(reader, state);
 
-                return {
-                    cancel: function () {
-                        state.cancelled = true;
-                    }
-                };
-
-            });
-
-
+            return {
+                cancel: function () {
+                    state.cancelled = true;
+                }
+            };
         };
 
         var initializeState = function (config) {
 
-            return $http.get(config.getSasLink + encodeURIComponent(config.file.name))
-                .then(function (response) {
 
-                    var blockSize = DefaultBlockSize;
+            var blockSize = DefaultBlockSize;
 
-                    var maxBlockSize = blockSize; // Default Block Size
-                    var numberOfBlocks = 1;
+            var maxBlockSize = blockSize; // Default Block Size
+            var numberOfBlocks = 1;
 
-                    var file = config.file;
+            var file = config.file;
 
-                    var fileSize = file.size;
-                    if (fileSize < blockSize) {
-                        maxBlockSize = fileSize;
+            var fileSize = file.size;
+            if (fileSize < blockSize) {
+                maxBlockSize = fileSize;
 
-                    }
+            }
 
-                    if (fileSize % maxBlockSize == 0) {
-                        numberOfBlocks = fileSize / maxBlockSize;
-                    } else {
-                        numberOfBlocks = parseInt(fileSize / maxBlockSize, 10) + 1;
-                    }
+            if (fileSize % maxBlockSize == 0) {
+                numberOfBlocks = fileSize / maxBlockSize;
+            } else {
+                numberOfBlocks = parseInt(fileSize / maxBlockSize, 10) + 1;
+            }
 
 
-                    return {
-                        maxBlockSize: maxBlockSize, //Each file will be split in 256 KB.
-                        numberOfBlocks: numberOfBlocks,
-                        totalBytesRemaining: fileSize,
-                        currentFilePointer: 0,
-                        blockIds: new Array(),
-                        blockIdPrefix: 'block-',
-                        bytesUploaded: 0,
-                        submitUri: null,
-                        file: file,
-                        fileUrl: response.data.data.blob.data.blobBase + response.data.data.blob.data.blobUrl + response.data.data.blob.data.blobSASToken,
-                        progress: config.progress,
-                        complete: config.complete,
-                        error: config.error,
-                        cancelled: false
-                    };
+            return {
+                maxBlockSize: maxBlockSize, //Each file will be split in 256 KB.
+                numberOfBlocks: numberOfBlocks,
+                totalBytesRemaining: fileSize,
+                currentFilePointer: 0,
+                blockIds: new Array(),
+                blockIdPrefix: 'block-',
+                bytesUploaded: 0,
+                submitUri: null,
+                file: file,
+                fileUrl: response.sasUrl,
+                progress: config.progress,
+                complete: config.complete,
+                error: config.error,
+                cancelled: false
+            };
 
-                }, function (err) {
-
-
-                });
 
 
         };
