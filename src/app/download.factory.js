bcb9d4eb9e2b18df77f4cb92a3c5f9b07b7b81cf
Guerric Tan Lim <gtlim@nimbyx.com>
2019-05-14 10:53:20 +0800

    Download as zip function

--- src/app/download.factory.js
+++ src/app/download.factory.js
@@ -1,11 +1,13 @@
 (function () {
     'use strict';
     var FileSaver = require('file-saver');
-    Factory.$inject = ['$q', '$http'];
+    Factory.$inject = ['$q', '$http', 'zipService'];
 
-    function Factory($q, $http) {
+    function Factory($q, $http, zipService) {
 
         var defer = $q.defer();
+       
+        
 
         function downloadAsFile(config) {
 
@@ -40,9 +42,85 @@
             return defer.promise;
         }
 
+        function downlaodAsZip(configs, callback, fileName) {
+            var deferredPromises = [];
+            var files = [];        
+            var progress = [];   
+
+            angular.forEach(configs, function (value) {
+                var fileInProgress = {
+                    "progress": 0,
+                    "filename": value.filename
+                };
+
+                progress.push(fileInProgress);
+            })
+
+            angular.forEach(configs, function(value) {
+                deferredPromises.push(downloadEachFile(value, callback, files, progress));
+            })
+
+            $q.all(deferredPromises)
+                .then(function(result) {
+                    zipService.zipMultipleBlobs(files, fileName)
+                        .then(function (response) {
+                            FileSaver.saveAs(response, fileName);
+                            defer.resolve();
+                        }, function(error) {
+                            defer.reject(error);
+                        })
+                })            
+
+            return defer.promise;
+        }
+
+        function downloadEachFile(config, callback, files, progress) {   
+                 
+            return $http.get(config.sasUrl, {
+                cache: false,
+                responseType: "arraybuffer",
+                eventHandlers: {
+                    progress: function (event) {
+                       var getProgress = progress.find(x => x.filename === config.filename);  
+                       getProgress.progress = (event.loaded / event.total) * 100; 
+                       callback(calculateAggregateProgress(progress));
+                    }
+                },
+                headers: {
+                    "Content-Type": "application/octet-stream; charset=utf-8"
+                },
+                requestComingFromUploaderService: true
+            }).then(function (response) {
+                var octetStreamMime = "application/octet-stream";
+
+                var contentType = response.headers["content-type"] || octetStreamMime;
+
+                var blob = new Blob([response.data], { type: contentType });
+
+                var file = {
+                    "blob": blob,
+                    "filename": config.filename
+                };
+                files.push(file);
+
+            }, function (response) {
+
+            });
+        }
+
+        function calculateAggregateProgress(progresses) {
+            var total = 0;
+
+            angular.forEach(progresses, function (value) {
+                total += value.progress;
+            })
+
+            return total/progresses.length;
+        }
+
         return {
             downloadAsFile: downloadAsFile,
-            //downlaodAsZip: downlaodAsZip,
+            downlaodAsZip: downlaodAsZip
             //downloadMutipleAsFiles: downloadMutipleAsFiles,
             //downloadMutipleAsZip: downloadMutipleAsZip,
             //downloadMutipleAsSingleZip: downloadMutipleAsSingleZip,
