590645cba5433913498ec713b3b895900bf741ef
gonzaleziantimothy <gonzaleziantimothy@gmail.com>
2020-01-20 07:58:16 +0800

    include new zip file function

--- src/app/app.js
+++ src/app/app.js
@@ -12,7 +12,7 @@
         .service('uploadService', uploadService)
         .service('downloadService', downloadService)
         .service('zipService', zipService)
-        .directive('browseFile', function () {
+        .directive('browseFile', ['$q', 'zipService', function ($q, zipService) {
             return {
                 template: '<input style="display: none;" id="{{input + $id}}" multiple type="file"/><label for="{{input + $id}}">browse</label>',
                 scope: {
@@ -29,34 +29,52 @@
                         if (files.length > 0) {
 
                             for (var i = 0; i < files.length; i++) {
-                                if (scope.imageOnly()) {
-                                    var extension = files[i].name.split(".").pop();
-
-                                    var isImage = false;
-                                    angular.forEach(supportedImageFormats, function (value, key) {
-                                        if (value.toUpperCase() === extension.toUpperCase()) {
-                                            isImage = true;
-                                        }
-                                    });
-
-                                    if (isImage) {
+                                var extension = files[i].name.split(".").pop();
+                                var promises = [];
+                                var isValidFile = true;
+                                if (extension == 'zip') {
+                                    promises.push(zipService.validateZipFile(files[i]).then(function (result) {
+                                        var isValidFile = result;
+                                        if (isValidFile) {
+                                            fileList.push(files[i]);
+                                        };
+                                    }));
+                                }
+                                else {
+                                    var isValidFile = zipService.validateFiles(files[i]);
+                                    if (isValidFile) {
                                         fileList.push(files[i]);
                                     }
                                 }
-                                else {
-                                    fileList.push(files[i]);
-                                }
+
+
+                                $q.all(promises).then(function () {
+                                    if (isValidFile && scope.imageOnly()) {
+
+                                        var isImage = false;
+                                        angular.forEach(supportedImageFormats, function (value, key) {
+                                            if (value.toUpperCase() === extension.toUpperCase()) {
+                                                isImage = true;
+                                            }
+                                        });
+
+                                        if (isImage) {
+                                            fileList.push(files[i]);
+                                        }
+                                    }
+                                    else {
+                                        fileList.push(files[i]);
+                                    }
+                                });
                             }
                         }
-                        console.log('not cleared', files);
                         scope.fileListCallback({ files: fileList });
-                        this.value = null
-                        console.log('cleared', files);
+                        this.value = null;
                     });
                 }
             };
 
-        })
+        }])
         .directive('dropFile', ['$q', 'zipService', function ($q, zipService) {
             return {
                 scope: {
@@ -83,6 +101,24 @@
 
                                 if (typeof (e.dataTransfer.items) !== 'undefined') {
                                     var entry = e.dataTransfer.items[i].webkitGetAsEntry();
+                                    var extension = entry.name.split(".").pop();
+
+                                    if (extension == 'zip') {
+                                        zipService.validateZipFile(files[i]).then(function (result) {
+                                            var validatedZipFile = result;
+                                            if (validatedZipFile = true) {
+                                                fileList.push(files[i]);
+                                            };
+                                        });
+                                    }
+                                    else {
+                                        var validatedFile = zipService.validateFiles(files[i]);
+                                        if (validatedFile = true) {
+                                            fileList.push(files[i]);
+                                        };
+                                    }
+
+
                                     if (scope.imageOnly()) {
                                         var extension = files[i].name.split(".").pop();
 
@@ -102,9 +138,9 @@
                                             fileList.push(files[i]);
                                         } else if (entry.isDirectory) {
                                             zipPromises.push(zipService.zipFolder(entry));
-                                        }   
-                                    }                             
-                                }                 
+                                        }
+                                    }
+                                }
                             }
                         }
 
@@ -115,7 +151,7 @@
                             scope.fileListCallback({ files: fileList });
                             zipPromises = [];
                         });
-                  
+
                     });
                 }
             };
